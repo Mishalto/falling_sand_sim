f7bfbbcf4d2ce089c1731393eee2769735f37ba5
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-20 01:27:06 +0300

    Explicit type casting

--- src/Core/Grain.cpp
+++ src/Core/Grain.cpp
@@ -3,6 +3,7 @@
 Grain::Grain() {
     grain_.setSize(GrainStats::size);
     grain_.setFillColor(GrainStats::color);
+    grain_.setPosition({static_cast<sf::Vector2f>(sf::Mouse::getPosition())});
 }
 
 sf::RectangleShape& Grain::get_grain() {
