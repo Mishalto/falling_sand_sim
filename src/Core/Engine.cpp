5cc97ba2c7c1fae490980de1c3c0d439d910be04
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-20 00:47:00 +0300

    Added mouse track

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -7,14 +7,25 @@ Engine::Engine() : window_(sf::VideoMode({Resolution::width, Resolution::height}
 void Engine::start() {
     while (window_.isOpen()) {
         check_events();
+        update();
         draw();
     }
 }
 
+void Engine::update() {
+    sand_.get_grain().move({1, 1});
+}
+
 void Engine::check_events() {
     while (const std::optional event = window_.pollEvent()) {
-        if (event->is<sf::Event::Closed>())
+        if (event->is<sf::Event::Closed>()) {
             window_.close();
+        }
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+        sf::Vector2f mouse_pos = static_cast<sf::Vector2f>(sf::Mouse::getPosition());
+        sand_.get_grain().setPosition({mouse_pos});
     }
 }
 
