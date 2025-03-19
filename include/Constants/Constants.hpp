4bdf332fff523ee34069b10f1e6678f09097bb2c
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-19 23:49:29 +0300

    Added grain data

--- include/Constants/Constants.hpp
+++ include/Constants/Constants.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 namespace Resolution {
     const int width = 1920;
     const int height = 1080;
+}
+
+namespace Grain {
+    const sf::Vector2f size = {100, 100};
+    const sf::Color color = sf::Color(194, 178, 128);
 }
