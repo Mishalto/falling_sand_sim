bdf8a4ab4d4d619023073bd97df29c104a4ba4c3
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-21 01:13:50 +0300

    Check the availability of the cell

--- src/Core/Sand.cpp
+++ src/Core/Sand.cpp
@@ -14,10 +14,12 @@ void Sand::add_grain(sf::Vector2i mouse_pos) {
     for (size_t i = 0; i < grid_.size(); ++i) {
         for (size_t j = 0; j <grid_[i].size(); ++j) {
             if (mouse_pos.x > j * GrainStats::size && mouse_pos.x < j * GrainStats::size + GrainStats::size &&  // check left side and right side
-                mouse_pos.y > i * GrainStats::size && mouse_pos.y < i * GrainStats::size + GrainStats::size) {  // check top and bottom
+                mouse_pos.y > i * GrainStats::size && mouse_pos.y < i * GrainStats::size + GrainStats::size &&  // check top and bottom
+                !grid_[i][j]) { //Check the availability of the cell
                     float x_pos = static_cast<int>(j) * GrainStats::size;
                     float y_pos = static_cast<int>(i) * GrainStats::size;
                     sand_pool_[i][j] = Grain{{x_pos, y_pos}};
+                    grid_[i][j] = true;
                 }
         }
     }
