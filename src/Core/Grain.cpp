abf7649ff6624ecb4bc1da35e2a1d0c2ce8f98dd
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-20 21:43:54 +0300

    delete setPosition in constr

--- src/Core/Grain.cpp
+++ src/Core/Grain.cpp
@@ -3,7 +3,6 @@
 Grain::Grain() {
     grain_.setSize(GrainStats::size_2f);
     grain_.setFillColor(GrainStats::color);
-    grain_.setPosition({static_cast<sf::Vector2f>(sf::Mouse::getPosition())});
 }
 
 sf::RectangleShape& Grain::get_grain() {
