55d38bca550012e92912265cb593bf2f7b5dcbb7
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-20 22:08:04 +0300

    spawn logic moved

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -26,20 +26,7 @@ void Engine::check_events() {
     }
 
     if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-        auto grid_ = sand_.get_grid();
-        for (size_t i = 0; i < grid_.size(); ++i) {
-            for (size_t j = 0; j < grid_[i].size(); ++j) {
-                sf::Vector2i mouse_pos = sf::Mouse::getPosition();
-                //  Cheking that the cursor in within the grid boundaries
-                if (mouse_pos.x > j * GrainStats::size && mouse_pos.x < j * GrainStats::size + GrainStats::size &&  // check left side and right side
-                    mouse_pos.y > i * GrainStats::size && mouse_pos.y < i * GrainStats::size + GrainStats::size) {  // check top and bottom
-                        float x_pos = static_cast<int>(j) * GrainStats::size;
-                        float y_pos = static_cast<int>(i) * GrainStats::size;
-                        sf::Vector2f pos = {x_pos, y_pos};
-                        sand_.add_grain(pos);
-                    }
-            }
-        }
+        sand_.add_grain(sf::Mouse::getPosition());
     }
 }
 
