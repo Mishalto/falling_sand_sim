03bb8a60c321a38c72dae4993a1613c84c5e1c04
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-29 01:01:26 +0300

    Rework and optimized spawn grains logic

--- src/Sand.cpp
+++ src/Sand.cpp
@@ -14,17 +14,11 @@ void Sand::init_grid() {
 }
 
 void Sand::add_grain(sf::Vector2i mouse_pos) {
-    for (size_t y = 0; y < grid_.size(); ++y) {
-        for (size_t x = 0; x < grid_[y].size(); ++x) {
-            if (mouse_pos.x >= x * GrainStats::size && mouse_pos.x <= x * GrainStats::size + GrainStats::size &&  // check left side and right side
-                mouse_pos.y >= y * GrainStats::size && mouse_pos.y <= y * GrainStats::size + GrainStats::size &&  // check top and bottom
-                grid_[y][x] == GrainState::Free) { // Check the availability of the cell
-                    float x_pos = static_cast<int>(x) * GrainStats::size;
-                    float y_pos = static_cast<int>(y) * GrainStats::size;
-                    sand_pool_.emplace_back(Grain{{x_pos, y_pos}, {static_cast<int>(x), static_cast<int>(y)}}); // vector2f(pos) / vector2i(coordinate)
-                    grid_[y][x] = GrainState::Occupied;
-                }
-        }
+    // Converts the mouse position to cell format.
+    sf::Vector2i grid_pos = {static_cast<int>(mouse_pos.x / GrainStats::size), static_cast<int>(mouse_pos.y / GrainStats::size)};
+    if (grid_[grid_pos.y][grid_pos.x] == GrainState::Free) {
+        sand_pool_.emplace_back(Grain{grid_pos});
+        grid_[grid_pos.y][grid_pos.x] = GrainState::Occupied;
     }
 }
 
@@ -44,7 +38,6 @@ void Sand::step()
         if (cd.y + step >= grid_.size() && grid_[cd.y][cd.x] != GrainState::Idle)
         {
             // the grain is resting on the bottom of the grid
-            // TODO mark the grain has resting, so that it does not need to be checked again
             continue;
         }
         if (grid_[cd.y + step][cd.x] == GrainState::Free)
