fe0c0ce82e9afbdd141ecbf62c734366ad6d23d5
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-29 11:27:19 +0300

    Grain resized

--- include/Constants/Constants.hpp
+++ include/Constants/Constants.hpp
@@ -15,7 +15,7 @@ namespace Memory {
 }
 
 namespace GrainStats {
-    constexpr float size = 5;
+    constexpr float size = 2;
     constexpr sf::Vector2f size_2f = {size, size};
     constexpr sf::Color color = sf::Color(194, 178, 128);
     constexpr uint8_t idle_threshold = 50;
