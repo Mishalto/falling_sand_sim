a0430b8f694516fdd68f8ca6fa92e87b21fedf35
JamesBremner <james@ravenspoint.com>
2025-03-30 17:27:58 -0400

    remove dead code

--- include/Sand.hpp
+++ include/Sand.hpp
@@ -11,22 +11,30 @@ class Sand
 {
 
 private:
+
+    /* Data Structure
+
+    A 2D vector containing shared pointers to the simulated grains of sand
+
+    */
     typedef std::shared_ptr<Grain> grain_t;
     typedef std::vector<std::vector<grain_t>> grid_t;
 
-    grid_t grid_;
+    grid_t grid_;       // storage of the sand grains
 
+    // Unblock the grains above a location from which a grain just left
     void freeGrainsAbove( const sf::Vector2i& location );
-    
+
 public:
     Sand();
 
+    /// move the grains downwards one step
     void update();
+
+    /// add sand grain at position of mouse cursor
     void add_grain(sf::Vector2i mouse_pos);
+
+    /// draw all the sand grains
     void draw( sf::RenderWindow& window);
 
-    grid_t &getGrid()
-    {
-        return grid_;
-    }
 };
