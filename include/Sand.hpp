456bee24b160940af37169323feafdb9b973a8e7
JamesBremner <james@ravenspoint.com>
2025-03-30 15:07:56 -0400

    grid 2D vector of grain pointers

--- include/Sand.hpp
+++ include/Sand.hpp
@@ -1,24 +1,26 @@
 #pragma once
 
-#include <array>
 #include <vector>
+#include <memory>
 
 #include <SFML/Graphics.hpp>
 
-#include <GrainState.hpp>
 #include <Grain.hpp>
 
-class Sand {
+class Sand
+{
 
 private:
-    std::vector<Grain> sand_pool_;
-    std::array<std::array<CellState, Grid::x_cells>, Grid::y_cells> grid_;
+    typedef std::shared_ptr<Grain> grain_t;
+
+    std::vector<std::vector<grain_t>> grid_;
 
     void init_grid();
+
 public:
     Sand();
 
     void update();
     void add_grain(sf::Vector2i mouse_pos);
-    std::vector<Grain>& get_grains();
+    std::vector<Grain> &get_grains();
 };
