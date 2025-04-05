0d414ba8da2f368b87acbc75bebc53d35fb9e99e
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-04-06 00:07:36 +0300

    Logic improved(may be)

--- include/Particle/Sand.hpp
+++ include/Particle/Sand.hpp
@@ -15,9 +15,9 @@ class Sand : public Particle {
     private:
         void move(const sf::Vector2i& dir);
 
-        bool bottom_is_free(const GridPtr& grid, const sf::Vector2i& cd) const;
-        bool bottom_right_is_free(const GridPtr& grid, const sf::Vector2i& cd) const;
-        bool bottom_left_is_free(const GridPtr& grid, const sf::Vector2i& cd) const;
+        bool bottom_is_free(const GridPtr& grid);
+        bool bottom_right_is_free(const GridPtr& grid);
+        bool bottom_left_is_free(const GridPtr& grid);
     public:
         // Constructor that initializes sand at the given coordinates (calls base class constructor)
         Sand(const sf::Vector2i& coord);
--- src/Particle/Sand.cpp
+++ src/Particle/Sand.cpp
@@ -18,18 +18,16 @@ void Sand::update(GridPtr& grid) {
         return;
     }
     // Check all directions
-    if (bottom_is_free(grid, cd)) {
+    if (bottom_is_free(grid)) {
         move({0, 1});   // To bottom
         grid[cd.y + 1][cd.x] = std::move(grid[cd.y][cd.x]);
 
-    } else if (bottom_right_is_free(grid, cd)) {
+    } else if (bottom_right_is_free(grid)) {
         move({1, 1});   // To right
         grid[cd.y + 1][cd.x + 1] = std::move(grid[cd.y][cd.x]);
-    } else if (bottom_left_is_free(grid, cd)) {
+    } else if (bottom_left_is_free(grid)) {
         move({-1, 1});  // To left
         grid[cd.y + 1][cd.x - 1] = std::move(grid[cd.y][cd.x]);
-    } else {
-        set_at_rest(true);
     }
 }
 
@@ -39,19 +37,53 @@ void Sand::move(const sf::Vector2i& dir) {
     get_coord() += {dir.x, dir.y};
 }
 
-// Checking availability of the bottom, bottom-left, and bottom-right.
-bool Sand::bottom_is_free(const GridPtr& grid, const sf::Vector2i& cd) const {
-    return (grid[cd.y + 1][cd.x] == nullptr) || (grid[cd.y + 1][cd.x]->get_type() == ParticleType::Water);
+// Checking availability of the bottom
+bool Sand::bottom_is_free(const GridPtr& grid) {
+    const auto cd = get_coord();
+    // Check if bottom cell is free
+    if (grid[cd.y + 1][cd.x] == nullptr) {
+        return true;
+    }
+    // Check if bottom cell is water
+    else if (grid[cd.y + 1][cd.x]->get_type() == ParticleType::Water) {
+        return true;
+    }
+
+    return false;
 }
-bool Sand::bottom_right_is_free(const GridPtr& grid, const sf::Vector2i& cd) const {
-    return (cd.x + 1 < grid[0].size() && grid[cd.y][cd.x + 1] == nullptr &&
-           grid[cd.y + 1][cd.x + 1] == nullptr) ||
-           (cd.x + 1 < grid[0].size() && grid[cd.y][cd.x + 1] != nullptr && grid[cd.y][cd.x + 1]->get_type() == ParticleType::Water &&
-           grid[cd.y + 1][cd.x + 1] != nullptr && grid[cd.y + 1][cd.x + 1]->get_type() == ParticleType::Water);
+// Cheking availability of the bottom-right
+bool Sand::bottom_right_is_free(const GridPtr& grid) {
+    const auto cd = get_coord();
+    // Check out of bounds
+    if (cd.y + 1 >= grid.size() || cd.x + 1 >= grid[0].size()) {
+        return false;
+    }
+    // Check if bottom-right is free
+    if (grid[cd.y + 1][cd.x + 1] == nullptr) {
+        return true;
+    }
+    // Check if bottom-right is water
+    else if (grid[cd.y + 1][cd.x + 1]->get_type() == ParticleType::Water) {
+        return true;
+    }
+
+    return false;
 }
-bool Sand::bottom_left_is_free(const GridPtr& grid, const sf::Vector2i& cd) const {
-    return (cd.x - 1 >= 0 && grid[cd.y][cd.x - 1] == nullptr &&
-           grid[cd.y + 1][cd.x - 1] == nullptr) || 
-           (cd.x - 1 >= 0 && grid[cd.y][cd.x - 1] != nullptr && grid[cd.y][cd.x - 1]->get_type() == ParticleType::Water &&
-            grid[cd.y + 1][cd.x - 1] != nullptr && grid[cd.y + 1][cd.x - 1]->get_type() == ParticleType::Water);
+// Cheking availability of the bottom-right
+bool Sand::bottom_left_is_free(const GridPtr& grid) {
+    const auto cd = get_coord();
+    // Check out of bounds
+    if (cd.y - 1 < 0 || cd.x - 1 < 0) {
+        return false;
+    }
+    // Check if bottom-left is free
+    if (grid[cd.y - 1][cd.x - 1] == nullptr) {
+        return true;
+    }
+    // Check if bottom-left is water
+    else if ((grid[cd.y - 1][cd.x - 1]->get_type() == ParticleType::Water)) {
+        return true;
+    }
+
+    return false;
 }
