2e3fc1c6ee033d661240a316358e97dbe4956774
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-04-06 00:56:40 +0300

    Comment improved

--- include/Particle/Particle.hpp
+++ include/Particle/Particle.hpp
@@ -9,12 +9,13 @@ class Particle {
     private:
         // Particle shape
         sf::RectangleShape particle_;
-        // Particle position
+        // Particle position in grid coordinates
         sf::Vector2i coordinate_;
-        // Type of Part
+        // Type of particle(e.g Sand, Water)
         Type type_;
-        // True if particle is not moving
+        // Flag indicating whether the particle has stopped moved
         bool is_at_rest_;
+        // Flag indicating whether the particle has moved in the current update
         bool is_moved_;
 
     public:
@@ -22,19 +23,20 @@ class Particle {
         Particle(const sf::Vector2i& coord);
         // Moves the particle (must be implemented)
         virtual void update(GridPtr& grid) = 0;
-        // Returns the shape
+        // Returns the particle shape
         sf::RectangleShape& get_part();
-        // Returns the position
+        // Returns the position in grid coordinates
         sf::Vector2i& get_coord();
-        // Sets rest state
+        // Set is_at_rest_ based on the cond
         void set_at_rest(bool cond);
         // Checks if at rest
         bool is_at_rest() const;
-
+        // Set is_moved_ variable based on the cond
         void set_is_move(bool cond);
-
+        // Check is moved
         bool is_moved() const;
-
+        // Set type based on func parameter
         void set_type(Type type);
+        // Return particular type
         Type get_type() const;
     };
