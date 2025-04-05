b77d5f057a22d82760eba49f931b12974502c07d
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-04-06 01:05:33 +0300

    Commented

--- include/Particle/Sand.hpp
+++ include/Particle/Sand.hpp
@@ -12,10 +12,13 @@ class Sand : public Particle {
     using GridPtr = std::vector<std::vector<std::shared_ptr<Particle>>>;
 
     private:
+        // Move the particle in the given direction
         void move(const sf::Vector2i& dir);
-
+        // Checking availability of the bottom
         bool bottom_is_free(const GridPtr& grid);
+        // Checking availability of the bottom-right
         bool bottom_right_is_free(const GridPtr& grid);
+        // Checking availability of the bottom-left
         bool bottom_left_is_free(const GridPtr& grid);
     public:
         // Constructor that initializes sand at the given coordinates (calls base class constructor)
